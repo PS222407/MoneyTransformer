7614efc929341cce9041be0baeda06a8ec84a402
Jens Ramakers <74543766+PS222407@users.noreply.github.com>
2022-10-31 17:21:25 +0100

    Update MoneyTransformer.php

--- src/MoneyTransformer.php
+++ src/MoneyTransformer.php
@@ -5,12 +5,12 @@ trait MoneyTransformer
     public function moneyMollie($price): string
     {
 
-        return $this->moneyView(price: (int)round($price), seperator: '.', decimalsReplacement: '00', threeDigitSeperator: '', currencySymbol: '', spaceAfterSymbol: false, symbolAtEnd: false);
+        return $this->moneyView(price: (int)round($price), separator: '.', decimalsReplacement: '00', threeDigitSeperator: '', currencySymbol: '', spaceAfterSymbol: false, symbolAtEnd: false);
     }
 
     public function percentageView($percentage): string
     {
-        return $this->moneyView(price: (int)round($percentage), seperator: ',', decimalsReplacement: '00', threeDigitSeperator: '', currencySymbol: '%', spaceAfterSymbol: true, symbolAtEnd: false);
+        return $this->moneyView(price: (int)round($percentage), separator: ',', decimalsReplacement: '00', threeDigitSeperator: '', currencySymbol: '%', spaceAfterSymbol: true, symbolAtEnd: false);
     }
 
     /**
@@ -70,7 +70,7 @@ trait MoneyTransformer
     /**
      * formats integer as cents to (€ 9,25, € 9,-) using ISO 4217 standards
      * @param int $price price in integer (925 = € 9,25)
-     * @param string|null $seperator use , . or something else to divide the cents
+     * @param string|null $separator use , . or something else to divide the cents
      * @param string|null $decimalsReplacement replace double null (00) decimals to some custom string like - (5,-)
      * @param string|null $threeDigitSeperator seperates every 3 digits with a character e.g. whitespace (1 000 000)
      * @param string|null $currencySymbol
@@ -78,14 +78,20 @@ trait MoneyTransformer
      * @param bool|null $symbolAtEnd
      * @return string
      */
-    public function moneyView($price, string $seperator = null, string $decimalsReplacement = null, string $threeDigitSeperator = null, string $currencySymbol = null, bool $spaceAfterSymbol = null, bool $symbolAtEnd = null): string
+    public function moneyView($price, string $separator = null, string $decimalsReplacement = null, string $threeDigitSeperator = null, string $currencySymbol = null, bool $spaceAfterSymbol = null, bool $symbolAtEnd = null): string
     {
-        $seperator = $seperator !== null ? $seperator : ',';
+        $separator = $separator !== null ? $separator : ',';
         $decimalsReplacement = $decimalsReplacement !== null ? $decimalsReplacement : '00';
         $threeDigitSeperator = $threeDigitSeperator !== null ? $threeDigitSeperator : '.';
         $currencySymbol = $currencySymbol !== null ? $currencySymbol : '€';
         $spaceAfterSymbol = $spaceAfterSymbol !== null ? $spaceAfterSymbol : true;
         $symbolAtEnd = $symbolAtEnd !== null ? $symbolAtEnd : false;
+
+        $positive = true;
+        if ($price < 0) {
+            $price *= -1;
+            $positive = false;
+        }
 
         $priceStr = strval((int)round($price));
         $arr = str_split($priceStr);
@@ -95,7 +101,7 @@ trait MoneyTransformer
             $arr = str_split($priceStr);
         }
 
-        array_splice($arr, count($arr) - 2, 0, $seperator);
+        array_splice($arr, count($arr) - 2, 0, $separator);
 
         $count = count($arr);
         if ($arr[$count -2] == 0 && $arr[$count -1] == 0) {
@@ -105,7 +111,7 @@ trait MoneyTransformer
         }
 
         $implodeShit = implode('', $arr);
-        $splitComma = explode($seperator, $implodeShit);
+        $splitComma = explode($separator, $implodeShit);
 
         $modulo = strlen($splitComma[0]) % 3;
         $chunk1 = implode('', array_slice(str_split($splitComma[0]), 0, $modulo));
@@ -116,6 +122,7 @@ trait MoneyTransformer
         }
 
         $threeDigtSeperator = !$chunk2 ? '' : $threeDigitSeperator;
-        return ($symbolAtEnd ? '' : $currencySymbol) . ($spaceAfterSymbol ? ' ' : '') . $chunk1 . ($chunk1 === '' ? '' : $threeDigtSeperator) . implode($threeDigitSeperator, $chunk2) . (!empty($splitComma[1]) ? $seperator : $splitComma[1]) . $splitComma[1] . ($symbolAtEnd && $spaceAfterSymbol ? ' ' : '') . ($symbolAtEnd ? $currencySymbol : '');
+
+        return ($symbolAtEnd ? '' : $currencySymbol) . ($spaceAfterSymbol ? ' ' : '') . ($positive ? '' : '-') . $chunk1 . ($chunk1 === '' ? '' : $threeDigtSeperator) . implode($threeDigitSeperator, $chunk2) . (!empty($splitComma[1]) ? $separator : $splitComma[1]) . $splitComma[1] . ($symbolAtEnd && $spaceAfterSymbol ? ' ' : '') . ($symbolAtEnd ? $currencySymbol : '');
     }
 }
